852d834c93b1f913850f982b8fa0422e643bbdc9
as3mbus <asembus@gmail.com>
2017-05-23 12:19:48 +0700

    sorry for not commiting for every change

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,34 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home')->with('name', 'Stranger');
 });
+
+Route::get('/dev', function () {
+    $quest = DB::table('quest')->get();
+    // return $quest;
+    // return view('home2',
+    // ['quest'=> $quest,
+    //  'name'=> 'stranger']);
+    return view('home2');
+});
+
+Route::get('/quest', function () {
+    $quest = DB::table('quest')->get();
+    return view('quest.index')
+    ->with('quest', $quest)
+    ->with('name', "Stranger");
+});
+
+Route::get('/quest/{id}', function ($id) {
+    // dd($id);
+    $quest = DB::table('quest')->find($id);
+    // dd($quest);
+    return view('quest.show')
+    ->with('quest', $quest)
+    ->with('name', "Stranger");
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
